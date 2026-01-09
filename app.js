13085b86fea5ab4257eaa68f385d3e9fedfba1ff
Adebayo-png <adebayomuhammad40@gmail.com>
2026-01-09 18:51:50 +1300

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+  const loader = document.getElementById("loader");
+  const content = document.getElementById("content");
+
+
+
+  setTimeout(() => {
+    loader.style.display = "none";
+    content.style.display = "block";
+  }, 1);
+}
+  
+const display = document.getElementById("display");
+
+ function append(value) {
+  display.value += value;
+};
+
+function clearDisplay() {
+  display.value = "";
+};
+
+function calculate() {
+  display.value = eval(display.value);
+};
+
+function toggleMode () {
+  document.body.classList.toggle("dark");
+};
+
+
